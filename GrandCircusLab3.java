5bdc2aae434a6289411b059a8077647145d26b28
hebarber1 <hbm1@yahoo.com>
2017-10-26 09:19:01 -0400

    third commit

--- GrandCircusLab3.java
+++ GrandCircusLab3.java
@@ -7,42 +7,41 @@ public class GrandCircusLab3 {
 
 		// Have user input integer
 		int num = 0;
+		String response = "y";
 		Scanner scnr = new Scanner(System.in);
-		
-		System.out.println("Enter an integer:");
-		
-		num = scnr.nextInt();
-		
-		// Print title of columns
-		System.out.println("Number\t\tSquared\t\tCubes\n");
-		System.out.println("======\t\t======\t\t=====\n");
-		
+
 		// Create formulas for calculating squares and cubes
-		
+
 		// start for loop
-		
-		for (int i = 1; i <= num; i++) {
-		int userSquares = i * i ;
-		int userCubes = getCubes(i);
-		
-		// Based on user input display table for squared and cubed 
-		
-		
-		System.out.println(i + "\t\t" + userSquares + "\t\t" + userCubes);
-		
-		// Then ask user if they want to continue (y/n)
-		
+
+		while (response.equalsIgnoreCase("y")) {
+			System.out.println("Enter an integer:");
+			num = scnr.nextInt();
+			scnr.nextLine();
+			// Print title of columns
+			System.out.println("Number\t\tSquared\t\tCubes\n");
+			System.out.println("======\t\t======\t\t=====\n");
+			for (int i = 1; i <= num; i++) {
+				int userSquares = i * i;
+				int userCubes = getCubes(i);
+
+				// Based on user input display table for squared and cubed
+
+				System.out.println(i + "\t\t" + userSquares + "\t\t" + userCubes);
+			}
+			System.out.println("Continue?(y/n)");
+			response = scnr.nextLine();
 		}
-		
-		
-		
-		
+
+		System.out.println("Goodbye!");
 		scnr.close();
+
 	}
-		public static int getCubes(int num) {
-		
-			int result = (int) Math.pow(num, 3);
-			
-		return result;	
-		}
+
+	public static int getCubes(int num) {
+
+		int result = (int) Math.pow(num, 3);
+
+		return result;
+	}
 }
